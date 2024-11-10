c4f92f4eb9ed111dcb6cc6fc4c28088f3d0195cc
vjcspy <mr.vjcspy@gmail.com>
2024-11-10 09:34:20 +0700

    chore

--- stock/src/main/java/com/vjcspy/spring/application/Application.java
+++ stock/src/main/java/com/vjcspy/spring/application/Application.java
@@ -1,20 +1,17 @@
 /* (C) 2024 */
 package com.vjcspy.spring.application;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+import static net.logstash.logback.argument.StructuredArguments.value;
+
 import com.vjcspy.eventmanager.EventManager;
 import com.vjcspy.spring.tedbed.eventmanager.Actions;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-
-import java.util.Map;
-
-import static net.logstash.logback.argument.StructuredArguments.kv;
-import static net.logstash.logback.argument.StructuredArguments.value;
-
-//import static net.logstash.logback.argument.StructuredArguments.*;
 
 @SpringBootApplication(scanBasePackages = "com.vjcspy.spring")
 @Slf4j
@@ -25,24 +22,18 @@ public class Application {
 
     @EventListener(ApplicationReadyEvent.class)
     public void runAfterStartup() {
-        log.info("Processing payment",
-                kv("payment", Map.of(
-                        "id", 123,
-                        "amount", 45,
-                        "method", 234,
-                        "status", 123,
-                        "metadata", 44
-                ))
-        );
-        log.info("Processing order",
-                kv("orderId", "aa"),
-                kv("customerId", "bb"),
-                kv("amount", "ccc")
-        );
-        log.info("Order {} processed for customer {}",
-                value("orderId", "123"),
-                value("customerId", "456")
-        );
+        log.info(
+                "Processing payment",
+                kv(
+                        "payment",
+                        Map.of(
+                                "id", 123,
+                                "amount", 45,
+                                "method", 234,
+                                "status", 123,
+                                "metadata", 44)));
+        log.info("Processing order", kv("orderId", "aa"), kv("customerId", "bb"), kv("amount", "ccc"));
+        log.info("Order {} processed for customer {}", value("orderId", "123"), value("customerId", "456"));
         testRxEvent();
     }
 
