f5169a8c28fc59d49dce31b8641bdea4117e696b
vjcspy <mr.vjcspy@gmail.com>
2024-12-08 21:23:28 +0700

    feat: rabbitmq consumer with coroutine

--- projects/stock/src/main/kotlin/com/vjcspy/spring/packages/example/queue/RabbitExampleConsumer.kt
+++ projects/stock/src/main/kotlin/com/vjcspy/spring/packages/example/queue/RabbitExampleConsumer.kt
@@ -2,6 +2,8 @@ package com.vjcspy.spring.packages.example.queue
 
 import com.vjcspy.spring.packages.example.cfg.RabbitExampleConfig
 import kotlin.random.Random
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.context.annotation.Configuration
 
@@ -10,9 +12,11 @@ class RabbitExampleConsumer {
     // Consumer lắng nghe thông điệp từ queue "myQueue"
     @RabbitListener(queues = [RabbitExampleConfig.EXAMPLE_QUEUE], containerFactory = "listenerContainerFactory")
     fun consumeMessage(message: String) {
-        println("Received message: $message")
-        // Giả lập xử lý thông điệp
-        Thread.sleep(Random.nextInt(1000, 5001).toLong()) // Giả lập công việc xử lý mất 1 giây
-        println("Done message: $message")
+        runBlocking {
+            println("Received message: $message")
+            // Giả lập xử lý thông điệp
+            delay(Random.nextInt(1000, 5001).toLong()) // Giả lập công việc xử lý mất 1 giây
+            println("Done message: $message")
+        }
     }
 }
