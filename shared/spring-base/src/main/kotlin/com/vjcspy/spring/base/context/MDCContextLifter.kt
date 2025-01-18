56f0028083848ab6ad6094f15b21333462657fc8
ubuntu-desktop <mr.vjcspy@gmail.com>
2025-01-18 23:00:39 +0700

    chore

--- shared/spring-base/src/main/kotlin/com/vjcspy/spring/base/context/MDCContextLifter.kt
+++ shared/spring-base/src/main/kotlin/com/vjcspy/spring/base/context/MDCContextLifter.kt
@@ -6,8 +6,6 @@ import reactor.util.context.Context
 class MDCContextLifter<T>(
     private val subscriber: CoreSubscriber<T>,
 ) : CoreSubscriber<T> {
-    private val mdcContext: Map<String, String>? = MDC.getCopyOfContextMap()
-
     override fun currentContext(): Context = subscriber.currentContext()
 
     override fun onSubscribe(subscription: Subscription) {
@@ -15,52 +13,26 @@ class MDCContextLifter<T>(
     }
 
     override fun onNext(t: T) {
-        updateMdc(mdcContext)
-        try {
-            subscriber.onNext(t)
-        } finally {
-            restoreMdc(mdcContext)
-        }
+        updateMdc()
+        subscriber.onNext(t)
     }
 
     override fun onError(throwable: Throwable) {
-        updateMdc(mdcContext)
-        try {
-            subscriber.onError(throwable)
-        } finally {
-            restoreMdc(mdcContext)
-        }
+        updateMdc()
+        subscriber.onError(throwable)
     }
 
     override fun onComplete() {
-        updateMdc(mdcContext)
-        try {
-            subscriber.onComplete()
-        } finally {
-            restoreMdc(mdcContext)
-        }
+        updateMdc()
+        subscriber.onComplete()
     }
 
-    private fun updateMdc(context: Map<String, String>?) {
-        if (context != null) {
-            MDC.setContextMap(context)
-        } else {
-            MDC.clear()
-        }
-
+    private fun updateMdc() {
         // Copy context from Reactor Context to MDC
         currentContext().forEach { key, value ->
             if (key is String && value is String) {
                 MDC.put(key.toString(), value.toString())
             }
-        }
-    }
-
-    private fun restoreMdc(context: Map<String, String>?) {
-        if (context != null) {
-            MDC.setContextMap(context)
-        } else {
-            MDC.clear()
         }
     }
 }
